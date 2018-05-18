d7a4cfbb6c3afa2251e14e97d8858ba21c34f72e
Saranyan Senthivel <ssenthivel@jmhcompanies.com>
2018-05-18 17:09:42 -0500

    Updated Fizzbuzz

--- Fizzbuzz/Fizzbuzz/FizzBuzz.cs
+++ Fizzbuzz/Fizzbuzz/FizzBuzz.cs
@@ -10,26 +10,48 @@ namespace Fizzbuzz
     {
         static void Main(string[] args)
         {
-            for(int i =1; i<= 100; i++)
+            List<int> count3 = new List<int>();
+            List<int> count5 = new List<int>();
+            List<int> count35 = new List<int>();
+
+            for (int i =1; i<= 100; i++)
             {
                 string str = "";
                 if (i % 3 == 0)
                 {
                     str += "Fizz";
+                    count3.Add(i);
                 }
 
                 if (i % 5 == 0)
                 {
                     str += "Buzz";
+                    count5.Add(i);
                 }
 
                 if (str.Length == 0)
                 {
                     str = i.ToString();
                 }
+
+                if (str == "FizzBuzz")
+                {
+                    count35.Add(i);
+                }
                 Console.WriteLine(str);
-                Console.ReadLine();
+                
             }
+            count3.ForEach(item => Console.Write(item + ","));
+            Console.WriteLine();
+            count5.ForEach(item => Console.Write(item + ","));
+            Console.WriteLine();
+            count35.ForEach(item => Console.Write(item + ","));
+            Console.WriteLine();
+            Console.WriteLine("Average of Fizz {0}", count3.Average().ToString());
+            
+            Console.WriteLine("Average of Buzz {0}", count5.Average().ToString());
+            Console.WriteLine("Average of FizzBuzz {0}", count35.Average().ToString());
+            Console.ReadLine();
         }
     }
 }
